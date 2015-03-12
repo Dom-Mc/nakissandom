9d5203878ce39e06d37054d3cc406b50a643101e
Dom McKellar <dom.mc@outlook.com>
2015-03-12 07:15:37 -0700

    thankyou page edit

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -6,7 +6,9 @@
 <div class="element row">
 	<div class="large-8 large-centered columns end">
 		<h1 id="thx" class="text-center">Thank You!</h1>
-    	<p id="lead" class="text-center ">Look for your invitation to arrive in May 
+    	<p id="lead" class="text-center ">Look for your invitation to arrive in June.</br>
+    	Wedding updates, lodging, and travel information</br>
+    	will be available at this site. 
     	<i class="fa fa-paper-plane-o fa-lg"></i> </p>
 	</div>
 </div>
