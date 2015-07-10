2803d9126acf3dd2d2f1e9b700bd4e3a92c625f1
Dom <Dom-Mc@users.noreply.github.com>
2015-07-09 19:05:39 -0700

    Update thankyou.blade.php
    Date change

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -6,7 +6,7 @@
 <div class="element row">
 	<div class="large-8 large-centered columns end">
 		<h1 id="thx" class="text-center">Thank You!</h1>
-    	<p id="lead" class="text-center ">Look for your invitation to arrive in June. <i class="fa fa-paper-plane-o fa-lg"></i> </br>
+    	<p id="lead" class="text-center ">Look for your invitation to arrive in late July/early August. <i class="fa fa-paper-plane-o fa-lg"></i> </br>
     	Wedding updates, lodging, and travel information</br>
     	will be available at this site. 
     	</p>
